e24609b38a6238d62f994a49fe02419da7868df9
Love4653 <Kaiya.park@gmail.com>
2016-03-09 16:53:28 -0800

    Hi

---
+++ src/One.java
@@ -0,0 +1,16 @@
+public class One {
+
+	public static void main(String[] args) {
+		int num = 0;
+
+		for (int i = 0; i < 3; i++) {
+			System.out.print("\n");
+			for (int j = 0; j < 3; j++) {
+				num++;
+				System.out.print(num);
+			}
+		}
+
+	}
+
+}
---
+++ src/Two.java
@@ -0,0 +1,11 @@
+
+public class Two {
+	public static void main(String[] args) {
+		for (int i = 0; i < 3; i++) {
+			System.out.print("\n");
+			for (int j = 0; j < 3; j++) {
+				System.out.print(0);
+			}
+		}
+	}
+}
